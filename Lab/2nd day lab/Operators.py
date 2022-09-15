994a4de7ea0506e9000d06fd98d51911ba5c8e7b
Anmol Shrestha <anmolstha013@gmail.com>
2022-09-15 13:33:36 +0545

    Update

---
+++ Lab/2nd day lab/Operators.py
@@ -0,0 +1,26 @@
+# Result processing
+
+print("-------------------------------------------")
+
+# input
+student_id = input("Enter your SID : ")
+student_name = input("Enter your NAME : ")
+sub1 = int(input("Enter your marks in first subject : "))
+sub2 = int(input("Enter your marks in second subject : "))
+sub3 = int(input("Enter your marks in third subject : "))
+
+# Processing
+total = sub1 + sub2 + sub3
+average = total/3
+
+#Output
+print("Total : {}".format(total))
+print("Average : {}".format(average))
+
+if sub1>=40 and sub2>=40 and sub3>=40 :
+	print("Result : Pass")
+else :
+	print("Result : Fail")
+
+
+print("-------------------------------------------")
---
+++ Lab/2nd day lab/Variables.py
@@ -0,0 +1,12 @@
+#sid = None
+#print(id(sid))
+#sid = True
+#print(id(sid))
+
+num1 = 10
+num2 = 11
+num3 = 12
+
+print(id(num1))
+print(id(num2))
+print(id(num3))
